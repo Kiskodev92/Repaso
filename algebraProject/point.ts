7ff08c93db7a5c74c147359774fedd0aa0be3b12
Kiskotkd <fjgp92@hotmail.com>
2023-07-09 18:14:13 +0200

    point

---
+++ algebraProject/point.ts
@@ -0,0 +1,21 @@
+class Point {
+    private x : number;
+    private y : number;
+    constructor(x : number, y : number){
+        this.x = x;
+        this.y = y;
+    }
+    public setx(newx : number):void{
+        this.x= newx;
+    }
+    public getx() :number{
+        return this.x
+    }
+        public sety(newy : number):void{
+        this.y= newy;
+    }
+    public gety() :number{
+        return this.y
+    }
+    toString(){}
+}
